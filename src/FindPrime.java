aaa3bc2e2f4a7a43d538b24d1a3c7f5c43dcbf90
kdh0981 <kdh0981@barunsw.com>
2019-10-16 21:18:21 +0900

    소수 알고리즘 수정

--- src/FindPrime.java
+++ src/FindPrime.java
@@ -1,23 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class FindPrime {
   public int solution(int n) {
-    long start = System.currentTimeMillis();
-    int answer = 0;
+    List<Boolean> isPrimeList = new ArrayList<>();
+
+    // 0, 1 은 소수가 아님
+    isPrimeList.add(false);
+    isPrimeList.add(false);
 
     for ( int i = 2; i <= n; i++ ) {
-      boolean isPrime = true;
-
-      for ( int j = 2; j < i; j++ ) {
-        if ( i != j && i % j == 0 ) {
-          isPrime = false;
-          break;
-        }
-      }
-
-      if ( isPrime ) answer++;
+      // 에라토스테네스의 체 생성
+      isPrimeList.add(true);
     }
 
-    long end = System.currentTimeMillis();
-    System.out.println("time:::" + (end - start) + "ms");
-    return answer;
+    for ( int i = 2; i <= n; i++ ) {
+      int j = 2;
+      while (i * j <= n) {
+        if ( isPrimeList.get(i*j) ) { // 효율성 point: 앞의 배수로 처리된 것은 다시 처리하지 않는다.
+          isPrimeList.set(i * j, false);
+        }
+        j++;
+      }
+    }
+
+    return  ((Long)isPrimeList.stream().filter(isPrime -> isPrime).count()).intValue();
   }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-//        System.out.println(new FindPrime().solution(100));
-        System.out.println(new Marathon().solution(
-          new String[]{"mislav", "stanko", "mislav", "ana"},
-          new String[]{"stanko", "ana", "mislav"}));
+        System.out.println(new FindPrime().solution(10));
+//        System.out.println(new Marathon().solution(
+//          new String[]{"mislav", "stanko", "mislav", "ana"},
+//          new String[]{"stanko", "ana", "mislav"}));
     }
 }
